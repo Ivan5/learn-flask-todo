83c70fa6643c766ce953363f756ca76b46ce796f
Ivan5 <converse_ivan@hotmail.com>
2020-10-01 13:58:37 -0500

    make a register and login function

--- todo/__init__.py
+++ todo/__init__.py
@@ -6,10 +6,10 @@ def create_app():
   app = Flask(__name__)
   app.config.from_mapping(
     SECRET_KEY='mikey',
-    DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
-    DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
-    DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
-    DATABASE=os.environ.get('FLASK_DATABASE')
+    DATABASE_HOST='localhost',
+    DATABASE_PASSWORD='',
+    DATABASE_USER='root',
+    DATABASE='todo_flask'
   )
 
   from . import db
@@ -17,8 +17,10 @@ def create_app():
   db.init_app(app)
 
   from . import auth
+  from . import todo
 
   app.register_blueprint(auth.bp)
+  app.register_blueprint(todo.bp)
 
   @app.route('/hola')
   def hola():
--- todo/auth.py
+++ todo/auth.py
@@ -1,7 +1,7 @@
 import functools
 
 from flask import (
-  Blueprint, flash, g, render_template, request, url_for, session
+  Blueprint, flash, g, render_template, request, url_for, session, redirect
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -17,7 +17,7 @@ def register():
     password = request.form['password']
     db, c = get_db()
     error = None
-    c.execute('select id from user where username = %s')
+    c.execute('select id from user where username = %s',(username,))
     if not username:
       error = 'Username es requerido'
     if not password:
@@ -49,6 +49,27 @@ def login():
     if error is None:
       session.clear()
       session['user_id'] = user['id']
-      return redirect(url_for('index'))
+      return redirect(url_for('todo.index'))
     flash(error)
   return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+  user_id = session.get('user_id')
+
+  if user_id is None:
+    g.user = None
+  else:
+    db, c = get_db()
+    c.execute(
+      'select * from user where id = %s',(user_id,)
+    )
+    g.user = c.fetchone()
+
+def login_required(view):
+  @functools.wraps(view)
+  def wrapped_view(**kwargs):
+    if g.user is None:
+      return redirect(url_for('auth.login'))
+    return view(**kargs)
+  return wrapped_view
